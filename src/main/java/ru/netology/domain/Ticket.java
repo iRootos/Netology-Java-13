4e2658377e0c4e55a4d8fd190910304ca8cf5a80
Rootos <rootos@hotmail.com>
2022-06-21 18:27:20 +0300

    update

--- src/main/java/ru/netology/domain/Ticket.java
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -4,24 +4,39 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Ticket implements Comparable<Ticket> {
     private int id;
     private int price;
-    private String DepartureAirport;
-    private String ArrivalAirport;
-    private String time;
+    private String departureAirport;
+    private String arrivalAirport;
+    private int time;
 
     @Override
     public int compareTo(Ticket otherTicket) {
-        if (price < otherTicket.price) {
+        if (price < otherTicket.getPrice()) {
             return -1;
         }
-        if (price > otherTicket.price) {
+        if (price > otherTicket.getPrice()) {
             return 1;
         }
         return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return id == ticket.id && price == ticket.price && time == ticket.time && departureAirport.equals(ticket.departureAirport) && arrivalAirport.equals(ticket.arrivalAirport);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, price, departureAirport, arrivalAirport, time);
     }
 }
--- src/test/java/ru/netology/domain/TicketManagerTest.java
+++ src/test/java/ru/netology/domain/TicketManagerTest.java
@@ -2,44 +2,69 @@ package ru.netology.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TicketManagerTest {
-    Ticket ticket1 = new Ticket(1, 1299, "SVO", "KZN", "1ч. 35мин.");
-    Ticket ticket2 = new Ticket(2, 2199, "VKO", "KZN", "1ч. 35мин.");
-    Ticket ticket3 = new Ticket(3, 5300, "SVO", "KZN", "2ч. 45мин.");
-    Ticket ticket4 = new Ticket(4, 2399, "SVO", "KZN", "4ч. 20мин.");
-    Ticket ticket5 = new Ticket(5, 5600, "LED", "KZN", "2ч. 15мин.");
+    Ticket ticket1 = new Ticket(1, 1299, "SVO", "KZN", 180);
+    Ticket ticket2 = new Ticket(2, 2199, "VKO", "KZN", 175);
+    Ticket ticket3 = new Ticket(3, 5300, "SVO", "KZN", 160);
+    Ticket ticket4 = new Ticket(4, 2399, "SVO", "KZN", 200);
+    Ticket ticket5 = new Ticket(5, 5600, "LED", "KZN", 175);
     TicketRepository repository = new TicketRepository();
     TicketManager manager = new TicketManager(repository);
 
-    @Test
-    public void ShouldFindAndSortTickets() {
+    @BeforeEach
+    public void setUp() {
         manager.add(ticket1);
         manager.add(ticket2);
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
+    }
+
+    @Test
+    public void ShouldFindById() {
+        Ticket actual = repository.findById(2);
+        Ticket expected = ticket2;
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShouldFindAndSortTickets() {
         Ticket[] actual = manager.searchFromTo("SVO", "KZN");
-        Ticket[] expected = {ticket1, ticket4, ticket3};
+        Ticket[] expected = new Ticket[]{ticket1, ticket4, ticket3};
         assertArrayEquals(expected, actual);
     }
 
     @Test
+    public void shouldFindOneTicket() {
+        Ticket[] actual = manager.searchFromTo("LED", "KZN");
+        Ticket[] expected = new Ticket[]{ticket5};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldNotFoundTicket() {
+        Ticket[] actual = manager.searchFromTo("SVO", "OMG");
+        Ticket[] expected = new Ticket[]{};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
     public void ShouldRemoveById() {
-        manager.add(ticket1);
-        manager.add(ticket2);
-        manager.add(ticket3);
         manager.removeById(2);
         Ticket[] actual = manager.getAll();
-        Ticket[] expected = {ticket1, ticket3};
+        Ticket[] expected = {ticket1, ticket3, ticket4, ticket5};
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void ShouldRemoveByIdNotExists() {
         assertThrows(NotFoundException.class, () -> {
-            manager.removeById(1);
+            manager.removeById(6);
         });
     }
 }
